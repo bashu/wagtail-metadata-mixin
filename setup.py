6d05425d7ec2e0c6af48c9e4356f59a697e34692
Tobias Englert <te@englert.it>
2018-02-17 20:21:53 +0200

    Temporarily remove code to create mo file

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ from setuptools import setup, find_packages
 
 
 # When creating the sdist, make sure the django.mo file also exists:
-if 'sdist' in sys.argv or 'develop' in sys.argv:
-    os.chdir('wagtailmetadata')
-    try:
-        from django.core import management
-        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
-    except ImportError:
-        if 'sdist' in sys.argv:
-            raise
-    finally:
-        os.chdir('..')
+# if 'sdist' in sys.argv or 'develop' in sys.argv:
+#     os.chdir('wagtailmetadata')
+#     try:
+#         from django.core import management
+#         management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+#     except ImportError:
+#         if 'sdist' in sys.argv:
+#             raise
+#     finally:
+#         os.chdir('..')
 
 
 def read(*parts):
