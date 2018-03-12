90b8d9e93136e9012d1b31cba84f0ac098428de8
dadafari <marinoni.dario@gmail.com>
2018-03-12 17:07:38 +0100

    simple update v2.0

--- test_urls.py
+++ test_urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 
-from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
     url(r'', include(wagtail_urls)),
